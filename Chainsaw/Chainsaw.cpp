7bc04a60e4d23cede3c3dc9fc36d09a5782153db
Shariar Papon <spaponwork@gmail.com>
2023-03-06 04:55:39 -0500

    started

---
+++ Chainsaw/Chainsaw.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "general_utils.h"
+
+int main()
+{
+	prompt_read_string("hello world!");
+}
+
---
+++ Chainsaw/general_utils.h
@@ -0,0 +1,4 @@
+#include <iostream>
+void log_msg(std::string);
+std::string prompt_read_string(std::string prompt);
+std::string read_string();
